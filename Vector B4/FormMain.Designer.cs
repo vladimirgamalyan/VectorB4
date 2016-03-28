d8017314bb5490218be11a7bef8b0d3515ee5e3d
Vladimir Gamalyan <12879954+vladimirgamalyan@users.noreply.github.com>
2016-03-28 17:15:54 +0700

    Cleanup project

--- Vector B4/FormMain.Designer.cs
+++ Vector B4/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Vector_B4
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
--- Vector B4/FormMain.cs
+++ Vector B4/FormMain.cs
@@ -12,9 +12,9 @@ using System.Windows.Forms;
 
 namespace Vector_B4
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
             loadFormValues();
