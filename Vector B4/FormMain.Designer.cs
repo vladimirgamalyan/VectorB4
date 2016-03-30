97add414a36793a3524e95fa91ce9f5ec59fdfd7
Vladimir Gamalyan <12879954+vladimirgamalyan@users.noreply.github.com>
2016-03-30 21:50:55 +0700

    Восстановлены имена файлов, ранее по ошибке переименованых.

--- Vector B4/FormMain.Designer.cs
+++ Vector B4/FormMain.Designer.cs
@@ -38,17 +38,25 @@
             this.numericUpDownStep = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownRetire = new System.Windows.Forms.NumericUpDown();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonCreateTap = new System.Windows.Forms.Button();
             this.buttonConvert = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.buttonFixReturn = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDownRepeats = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRepeatStep = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRadius)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSpeed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStep)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRetire)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRepeats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRepeatStep)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -206,6 +214,7 @@
             this.groupBox1.Controls.Add(this.numericUpDownRetire);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.numericUpDownStep);
+            this.groupBox1.Controls.Add(this.buttonCreateTap);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.numericUpDownSpeed);
             this.groupBox1.Controls.Add(this.label3);
@@ -214,53 +223,10 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(685, 276);
+            this.groupBox1.Size = new System.Drawing.Size(685, 283);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Параметры сканирования";
-            // 
-            // buttonCreateTap
-            // 
-            this.buttonCreateTap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonCreateTap.Location = new System.Drawing.Point(12, 294);
-            this.buttonCreateTap.Name = "buttonCreateTap";
-            this.buttonCreateTap.Size = new System.Drawing.Size(226, 48);
-            this.buttonCreateTap.TabIndex = 10;
-            this.buttonCreateTap.Text = "Создать код сканирования";
-            this.buttonCreateTap.UseVisualStyleBackColor = true;
-            this.buttonCreateTap.Click += new System.EventHandler(this.buttonCreateTap_Click);
-            // 
-            // buttonConvert
-            // 
-            this.buttonConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonConvert.Location = new System.Drawing.Point(244, 294);
-            this.buttonConvert.Name = "buttonConvert";
-            this.buttonConvert.Size = new System.Drawing.Size(241, 48);
-            this.buttonConvert.TabIndex = 11;
-            this.buttonConvert.Text = "Конвертировать облако точек";
-            this.buttonConvert.UseVisualStyleBackColor = true;
-            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(12, 356);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(416, 17);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "ООО \"Незабываемые Системы Автоматизации\" Москва. 2016";
-            // 
-            // buttonFixReturn
-            // 
-            this.buttonFixReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonFixReturn.Location = new System.Drawing.Point(491, 294);
-            this.buttonFixReturn.Name = "buttonFixReturn";
-            this.buttonFixReturn.Size = new System.Drawing.Size(206, 48);
-            this.buttonFixReturn.TabIndex = 13;
-            this.buttonFixReturn.Text = "Исправить возврат";
-            this.buttonFixReturn.UseVisualStyleBackColor = true;
-            this.buttonFixReturn.Click += new System.EventHandler(this.buttonFixReturn_Click);
+            this.groupBox1.Text = "Сканирование";
             // 
             // pictureBox1
             // 
@@ -273,20 +239,145 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // Form1
+            // buttonCreateTap
+            // 
+            this.buttonCreateTap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCreateTap.Location = new System.Drawing.Point(465, 223);
+            this.buttonCreateTap.Name = "buttonCreateTap";
+            this.buttonCreateTap.Size = new System.Drawing.Size(188, 48);
+            this.buttonCreateTap.TabIndex = 10;
+            this.buttonCreateTap.Text = "Создать код сканирования";
+            this.buttonCreateTap.UseVisualStyleBackColor = true;
+            this.buttonCreateTap.Click += new System.EventHandler(this.buttonCreateTap_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConvert.Location = new System.Drawing.Point(465, 22);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(188, 48);
+            this.buttonConvert.TabIndex = 11;
+            this.buttonConvert.Text = "Конвертировать облако точек";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(9, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(416, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ООО \"Незабываемые Системы Автоматизации\" Москва. 2016";
+            // 
+            // buttonFixReturn
+            // 
+            this.buttonFixReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFixReturn.Location = new System.Drawing.Point(465, 74);
+            this.buttonFixReturn.Name = "buttonFixReturn";
+            this.buttonFixReturn.Size = new System.Drawing.Size(188, 48);
+            this.buttonFixReturn.TabIndex = 13;
+            this.buttonFixReturn.Text = "Исправить возврат и добавить повторы";
+            this.buttonFixReturn.UseVisualStyleBackColor = true;
+            this.buttonFixReturn.Click += new System.EventHandler(this.buttonFixReturn_Click);
+            // 
+            // numericUpDownRepeats
+            // 
+            this.numericUpDownRepeats.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownRepeats.Location = new System.Drawing.Point(124, 22);
+            this.numericUpDownRepeats.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRepeats.Name = "numericUpDownRepeats";
+            this.numericUpDownRepeats.Size = new System.Drawing.Size(161, 46);
+            this.numericUpDownRepeats.TabIndex = 14;
+            this.numericUpDownRepeats.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRepeats.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDownRepeatStep
+            // 
+            this.numericUpDownRepeatStep.DecimalPlaces = 2;
+            this.numericUpDownRepeatStep.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownRepeatStep.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDownRepeatStep.Location = new System.Drawing.Point(124, 74);
+            this.numericUpDownRepeatStep.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownRepeatStep.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDownRepeatStep.Name = "numericUpDownRepeatStep";
+            this.numericUpDownRepeatStep.Size = new System.Drawing.Size(161, 46);
+            this.numericUpDownRepeatStep.TabIndex = 15;
+            this.numericUpDownRepeatStep.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.buttonFixReturn);
+            this.groupBox2.Controls.Add(this.buttonConvert);
+            this.groupBox2.Controls.Add(this.numericUpDownRepeatStep);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.numericUpDownRepeats);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 301);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(685, 134);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Конвертер";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(52, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Повторы";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(9, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Смещение (мм)";
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 382);
-            this.Controls.Add(this.buttonFixReturn);
+            this.ClientSize = new System.Drawing.Size(709, 462);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.buttonConvert);
-            this.Controls.Add(this.buttonCreateTap);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "FormMain";
             this.Text = "Полировка дисков";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRadius)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSpeed)).EndInit();
@@ -295,6 +386,10 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRepeats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRepeatStep)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -316,6 +411,11 @@
         private System.Windows.Forms.Button buttonConvert;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button buttonFixReturn;
+        private System.Windows.Forms.NumericUpDown numericUpDownRepeats;
+        private System.Windows.Forms.NumericUpDown numericUpDownRepeatStep;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
